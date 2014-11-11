79ed4c805180653cb194431495b5a06f7824c74f
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2014-11-11 05:46:48 +0000

    fixed sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-   def create
+  def create
     @user = User.new(user_params)
       
-      respond_to do |format|
-        if @user.save
-          session[:user_id] = @user.id
-          # Tell the UserMailer to send a welcome email after save
-          UserMailer.welcome_email(@user).deliver
-   
-          format.html { redirect_to(@user, notice: 'User was successfully created.') }
-          format.json { render json: @user, status: :created, location: @user }
-        else
-          format.html { render :new }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to movies_path
+    else
+      render :new
     end
   end
 
